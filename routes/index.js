6da4af64aa011fce8164df7c40eba74e5bb28cc8
Luis Cárcamo <lecarcamol@gmail.com>
2022-03-02 17:31:26 -0300

    Middleware semLogin.js criado

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const indexController = require('../controllers/indexController')
+const indexController = require('../controllers/indexController');
+const semLogin = require('../middlewares/semLogin');
 
 router.get('/', indexController.home);
-router.get('/login', indexController.login)
+router.get('/login', semLogin, indexController.login)
 router.post('/login', indexController.validaLogin)
 router.get('/logout', indexController.logout)
 
